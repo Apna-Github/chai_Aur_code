ece696b23cabe89836a9569b489e458fb291fab7
“[ram <[ramphapal03@gmail.com]>
2024-10-24 10:54:38 +0530

    Installation and Run first program

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <h1>Chai aur React | Ram
+       <p>Today i was learned About the React with the help of code with chai,</p>
+      <p>that was very thrilled Experience i have got.</p>
+      <h1>Thank you so much HiteshBhai....</h1>
+      </h1>
+   
+  );
+}
+
+export default App;
---
+++ 01vitareact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Chai Aur React With vite | Ram</h1>
+  )
+}
+
+export default App
